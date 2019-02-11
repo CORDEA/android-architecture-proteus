3a31f309f6923c9f7b6a7e70afffa5badbf815a7
Yoshihiro Tanaka <contact@cordea.jp>
2019-02-11 11:01:33 +0900

    switch toolbar

--- app/src/main/java/jp/cordea/kompas/MainActivity.kt
+++ app/src/main/java/jp/cordea/kompas/MainActivity.kt
@@ -9,7 +9,7 @@ import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
-import jp.cordea.kompas.ui.main.databinding.FragmentMainBinding
+import jp.cordea.kompas.databinding.ActivityMainBinding
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
@@ -17,14 +17,24 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
+    private val navController by lazy { findNavController(this, R.id.main_nav_fragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<FragmentMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val id = destination.id
+            when (id) {
+                R.id.detailFragment -> supportActionBar?.hide()
+                R.id.mainFragment -> supportActionBar?.show()
+            }
+        }
     }
 
-    override fun onSupportNavigateUp(): Boolean =
-            findNavController(this, R.id.main_nav_fragment).navigateUp()
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> = dispatchingAndroidInjector
 }
